a5e25f6c2c781138afe4c891aad8453afa54ebcc
spaceraiders0 <spaceraiders@tutanota.com>
2021-02-20 14:46:22 +0000

    Workspace widget made.

--- src/manager.py
+++ src/manager.py
@@ -1,6 +1,8 @@
 """The executable part of the file manager.
 """
 
+import widgets
 import settings
 
-print("Settings: ", settings.load_settings())
+manager_settings = settings.load_settings()
+print(widgets.workspace_list(1, manager_settings))
--- src/widgets.py
+++ src/widgets.py
@@ -1,11 +1,18 @@
 """Contains the code for all the widgets.
 """
 
+from colorama import Back
 
-def workspace_list(settings: dict) -> str:
+GREY = Back.GREEN
+RESET = Back.RESET
+
+
+def workspace_list(selection: int, settings: dict) -> str:
     """Creates and formats a string that contains the list of
-    workspaces.
+    workspaces, and highlights the currently selected workspace.
 
+    :param selection: the selected workspace. defaults to the first
+    :type selection: int
     :param settings: the settings that contains the list of
             workspaces to use.
     :type settings: dict
@@ -13,5 +20,21 @@ def workspace_list(settings: dict) -> str:
     :rtype: str
     """
 
-    pass
+    built_widget = ""
+    seperator = settings["workspace_seperator"]
+    workspaces = settings["workspaces"]
 
+    for index, workspace in enumerate(workspaces):
+        # Prevents the seperator from being appended on the last
+        # workspace in the list.
+        if index == len(workspaces) - 1:
+            seperator = ""
+
+        if index == selection:
+            built_widget += GREY + workspace + RESET + seperator
+        else:
+            built_widget += workspace + seperator
+
+    return built_widget
+
+    
