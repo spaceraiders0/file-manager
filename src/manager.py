f8a053166d3b8c908e3b003b49870f2f2207b232
spaceraiders0 <spaceraiders@tutanota.com>
2021-04-14 12:19:13 +0000

    Cleanup.

--- src/manager.py
+++
@@ -1,8 +0,0 @@
-"""The executable part of the file manager.
-"""
-
-import widgets
-import settings
-
-manager_settings = settings.load_settings()
-print(widgets.workspace_list(manager_settings, 1))
